a43f3e99a11d5eb84ebd431146cd29c2f5f1f325
Mass-hideo <massimo.viviano09@myhunter.cuny.edu>
2024-11-07 13:41:05 -0500

    made small tweaks

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ async function fetchData() {
 // Check for existing data in localStorage or fetch new data
 if (!people_data_object || Date.now() > people_data_object.timestamp) {
     fetchData().then((data) => {
-        localStorage.setItem("OpenNYC_Data", JSON.stringify({ data: data, timestamp: Date.now() + 60000 * 5 }));
+        localStorage.setItem("OpenNYC_Data", JSON.stringify({ data: data, timestamp: Date.now() + 60000 * 60 * 24 * 30 }));
         sortBySalary(data);
     });
 } else {
@@ -65,40 +65,45 @@ function sortBySalary(data) {
 // Append rows to table
 function appendToTable(object) {
     const row = document.createElement('tr');
+    const firstname = object.first_name.slice(0,1) + object.first_name.slice(1,object.first_name.length).toLowerCase();
+    const lastname = object.last_name.slice(0,1) + object.last_name.slice(1,object.last_name.length).toLowerCase();
     row.innerHTML = `
-        <td>${object.first_name || 'N/A'}</td>
-        <td>${object.last_name || 'N/A'}</td>
-        <td>${object.title_description || 'N/A'}</td>
-        <td>${object.base_salary || 'N/A'}</td>
-        <td>${object.agency_name || 'N/A'}</td>
+        <td>${firstname + " " + lastname}</td>
+        <td>${object.title_description}</td>
+        <td>${object.base_salary}</td>
+        <td>${object.agency_name}</td>
     `;
     tableBody.appendChild(row);
 }
 
 // Filter and sort functions
-function sortByLastName(letter) {
+// function sortByLastName(letter) {
+//     dissolveAnim('rgb(0, 0, 25)')
+//     const test = letter;
+//     const upper = test.toUpperCase()
+//     document.getElementById("tableBody").innerHTML = "";
+//     people_data_object.data.forEach(item => {
+//         if (item.last_name && item.last_name[0] === upper) {
+//             appendToTable(item);
+//         }
+//     });
+// }
+
+function sortByFirstName(name) {
     dissolveAnim('rgb(0, 0, 25)')
-    const test = letter;
+    const test = name;
     const upper = test.toUpperCase()
     document.getElementById("tableBody").innerHTML = "";
     people_data_object.data.forEach(item => {
-        if (item.last_name && item.last_name[0] === upper) {
+        if (item.first_name && item.first_name.slice(0,name.length)=== upper) {
             appendToTable(item);
         }
-    });
+        else if (item.last_name && item.last_name.slice(0,name.length)=== upper) {
+            appendToTable(item);
+        }
+    })
 }
 
-function sortByFirstName(letter) {
-    dissolveAnim('rgb(0, 0, 25)')
-    const test = letter;
-    const upper = test.toUpperCase()
-    document.getElementById("tableBody").innerHTML = "";
-    people_data_object.data.forEach(item => {
-        if (item.first_name && item.first_name[0] === upper) {
-            appendToTable(item);
-        }
-    });
-}
 
 function sortByAnnual() {
     dissolveAnim('rgb(0, 0, 25)')
@@ -240,7 +245,7 @@ document.querySelector('.Hourly').addEventListener("click", () => sortByHourly()
 
 
 let letterIQ = document.getElementById("letterIQ"); //Input field
-let filterFirst = document.getElementById("filterFirst");//submit button
+// let filterFirst = document.getElementById("filterFirst");//submit button
 let filterLast = document.getElementById("filterLast");
 let letter = "";
 filterFirst.addEventListener('click', (event) => {
@@ -410,164 +415,3 @@ function sortByNameValue() {
         highestByName.appendChild(person);
     }
 }
-// const url = 'https://data.cityofnewyork.us/resource/k397-673e.json?$limit=10000&$offset=0';
-// const tableBody = document.getElementById('tableBody');
-// const people_data = localStorage.getItem("OpenNYC_Data"); //localstorage
-// let people_data_object = null;//localstorage object
-// let tweaked_people_data = null;
-// if (people_data) { //if this exists
-//     people_data_object = JSON.parse(people_data) //then create the object value
-//     tweaked_people_data = structuredClone(people_data_object);
-// }
-
-// async function fetchData() {
-//     const response = await fetch(url)
-//     if (response.ok) {
-//         const data = await response.json();
-//         sortBySalary(data);
-//         return data
-//     }
-//     return [];
-// }
-// if (!people_data_object) { //if there is no local storage object created
-//     fetchData()
-//         .then((data) => {
-//             localStorage.setItem("OpenNYC_Data", JSON.stringify(data)) //create the local storage object, and turn it into a JSON
-//             //add expiry date 24hrs after this is loaded
-//             sortBySalary(data); //sort the salary in descending order of highest to lowest.
-//         })
-// }
-// else {
-//     sortBySalary(people_data_object);
-// }
-
-// function sortBySalary() {
-//     document.getElementById("tableBody").innerHTML = "";
-//     const sortedData = people_data_object.sort((a, b) => b.base_salary - a.base_salary);
-//     sortedData.forEach(item => {
-//         appendToTable(item);
-//     });
-// }
-
-// function appendToTable(object) {
-//     const row = document.createElement('tr');
-//     row.innerHTML = `
-//     <td>${object.first_name || 'N/A'}</td>
-//     <td>${object.title_description || 'N/A'}</td>
-//     <td>${object.base_salary || 'N/A'}</td>
-//     <td>${object.agency_name || 'N/A'}</td>
-// `;
-//     tableBody.appendChild(row);
-// }
-// //Function to sort by name
-// //Just need to pass a value
-// function sortByLastName() {
-//     console.log(people_data_object);
-//     document.getElementById("tableBody").innerHTML = "";
-//     //.sort((a, b) => return a.salary - b.salary)
-//     people_data_object.forEach(item => {
-//         if (item.last_name[0] == 'A') {
-//             appendToTable(item);
-//         }
-//     })
-// }
-
-// function sortByFirstName() {
-//     document.getElementById("tableBody").innerHTML = "";
-//     //.sort((a, b) => return a.salary - b.salary)
-//     people_data_object.forEach(item => {
-//         if (item.first_name[0] == 'A') {
-//             appendToTable(item);
-//         }
-//     })
-// }
-// //sorts table by only showing People who are salaried
-// function sortByAnnual() {
-//     document.getElementById("tableBody").innerHTML = "";
-//     document.getElementById("variable").innerHTML = "Annual Salary";
-//     people_data_object.forEach(item => {
-//         if (item.pay_basis == 'per Annum') {
-//             appendToTable(item);
-//         }
-//     })
-// }
-
-// //sorts table by only showing people who work hourly
-// function sortByHourly() {
-//     document.getElementById("tableBody").innerHTML = "";
-//     document.getElementById("variable").innerHTML = "Hourly Rate";
-//     //.sort((a, b) => return a.salary - b.salary)
-//     people_data_object.forEach(item => {
-//         if (item.pay_basis == 'per Hour') {
-//             appendToTable(item);
-//         }
-//     })
-// }
-
-// function FilterByBorough(boroughName) {
-//     document.getElementById("tableBody").innerHTML = "";
-//     console.log(boroughName);
-//     let i = 0;
-//     people_data_object.forEach(person => {
-//         if (person.work_location_borough === boroughName && i < 10) {
-//             appendToTable(person)
-//             console.log(person);
-//             i++
-//         }
-//         else if (i == 10) {
-//             return;
-//         }
-//     })
-// }
-
-// let brooklyn = document.querySelector('.Brooklyn');
-// let queens = document.querySelector('.Queens');
-// let manhattan = document.querySelector('.Manhattan');
-// let bronx = document.querySelector('.Bronx');
-// let statenIsland = document.querySelector('.StatenIsland');
-// let borough = "";
-// // Loop through each button and add the event listener
-
-// brooklyn.addEventListener("click", () => {
-//     FilterByBorough('BROOKLYN');
-// })
-
-// queens.addEventListener("click", () => {
-//     FilterByBorough('QUEENS');
-// })
-
-// manhattan.addEventListener("click", () => {
-//     FilterByBorough('MANHATTAN');
-// })
-
-// bronx.addEventListener("click", () => {
-//     FilterByBorough('BRONX');
-// })
-
-// statenIsland.addEventListener("click", () => {
-//     FilterByBorough('STATEN ISLAND');
-// })
-
-// // let buttons = document.querySelectorAll('button')
-
-// // buttons.forEach(button => {
-// //     button.addEventListener("click", () => {
-// //         document.getElementById("tableBody").innerHTML = "";
-// //         if (button.getElementsByClassName(".Brooklyn")) {
-// //             borough = "BROOKLYN";
-// //         }
-// //         else if (button.getElementsByClassName(".Queens")) {
-// //             borough = "QUEENS";
-// //         }
-// //         else if (button.getElementsByClassName(".Manhattan")) {
-// //             borough = "MANHATTAN";
-// //         }
-// //         else if (button.getElementsByClassName(".Bronx")) {
-// //             borough = "BRONX";
-// //         }
-// //         else {
-// //             borough = "STATEN ISLAND";
-// //         }
-// //         FilterByBorough(borough);
-// //     });
-// // });
